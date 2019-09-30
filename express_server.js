e588ad4c9f084932c4fd3a926c7b02fa544a91af
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-09-30 17:59:12 +0000

    Aded url show view

--- express_server.js
+++ express_server.js
@@ -16,11 +16,28 @@ app.get("/", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase }; //Must send as an object
+  res.render("urls_index", templateVars); //don't need extension or path since /views is a standard
+
+});
+app.get("/urls/:shortURL", (req, res) => {
+
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL]
+  }; //Must send as an object
+  console.log(req.params.shortURL);
+  console.log(urlDatabase[req.params.shortURL]);
+
+  res.render("urls_show", templateVars); //don't need extension or path since /views is a standard
+
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tiny app listening on port ${PORT}!`);
 });
