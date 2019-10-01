f316a595b9c8644d0df0916cfeaf2b3b13ae341c
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-10-01 19:59:23 +0000

    Added user specific features for adding/removing urls

--- express_server.js
+++ express_server.js
@@ -2,17 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require(`body-parser`);
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));//Parases the body of all requests as strings, and saves it as "requests.body"
-app.use(cookieParser())
+app.use(cookieParser());
 
 
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
 };
 
 const users = {
@@ -26,7 +26,7 @@ const users = {
     email: "user2@example.com",
     password: "dishwasher-funk"
   }
-}
+};
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -112,8 +112,22 @@ app.post("/logout", (req, res) => {
 app.post("/urls/:shortURL/delete", (req, res) => {
   //Deletes a url from from the list. Done on a POST request (not ideal)
   //Delete the shortURL entry from the database
+  console.log("Trying to delete");
+  let shortURL = req.params.shortURL;
+  //Check if the user is allowed to post
+  let userID = req.cookies["user_id"];
+  let user = findUserById(userID);
 
-  delete urlDatabase[req.params.shortURL]; //Deltes the short url
+  let filteredURLs = urlsForUser(userID);
+
+  if (!userID || !user || !filteredURLs.hasOwnProperty(shortURL)) {
+    //does not allow users to delete URLS they do not own. Users will only be able to reach this without logging in
+    // if a request is sent outide of the browser.
+    res.send("Please make sure you are logged in to delete a URL");
+    return;
+  }
+
+  delete urlDatabase[shortURL]; //Deltes the short url
   res.redirect("/urls");
 });
 
@@ -121,7 +135,19 @@ app.post("/urls/:shortURL", (req, res) => {
   //Renames a url
 
   let newURL = req.body.longURL;
-  urlDatabase[req.params.shortURL] = newURL; //Replaces the site
+  let shortURL = req.params.shortURL;
+  //Check if the user is allowed to post
+  let userID = req.cookies["user_id"];
+  let user = findUserById(userID);
+
+  let filteredURLs = urlsForUser(userID);
+
+  if (!userID || !user || !filteredURLs.hasOwnProperty(shortURL)) {
+    res.send("Please make sure you are logged in to submit a new url");
+    return;
+  }
+
+  urlDatabase[shortURL] = { longURL: newURL, userID }; //Replaces the site
   res.redirect("/urls");
 });
 
@@ -129,7 +155,16 @@ app.post("/urls/:shortURL", (req, res) => {
 app.get("/urls", (req, res) => {
   let userID = req.cookies["user_id"];
   let user = findUserById(userID);
-  let templateVars = { urls: urlDatabase, user };
+
+  if (!userID || !user) {
+    let templateVars = { urls: {}, user };
+    res.render("urls_index", templateVars);
+    return;
+  }
+
+
+  let filteredURLs = urlsForUser(userID);
+  let templateVars = { urls: filteredURLs, user };
 
   res.render("urls_index", templateVars);
 });
@@ -139,6 +174,11 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   let userID = req.cookies["user_id"];
   let user = findUserById(userID);
+  if (!userID || !user) {
+    res.redirect("/login");
+    return;
+  }
+
   let templateVars = { user };
 
   res.render("urls_new", templateVars);
@@ -149,12 +189,24 @@ app.get("/urls/:shortURL", (req, res) => {
 
   let userID = req.cookies["user_id"];
   let user = findUserById(userID);
+  let filteredURLs = urlsForUser(userID);
+  let templateVars = {};
+  if (filteredURLs && filteredURLs[req.params.shortURL]) { //If url exists
+    templateVars = {
+      shortURL: req.params.shortURL,
+      longURL: filteredURLs[req.params.shortURL],
+      user,
+    };//Must send as an object
 
-  let templateVars = {
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL],
-    user,
-  }; //Must send as an object
+  } else {
+    templateVars = {
+      shortURL: req.params.shortURL,
+      longURL: undefined,
+      user,
+    };
+  }
+
+
 
   res.render("urls_show", templateVars); //don't need extension or path since /views is a standard
 
@@ -165,27 +217,34 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   //displays all urls as a JSON
-  res.json(302, `/urls/${shortenedURL}`);
+  res.json(urlDatabase);
 });
 
 app.post("/urls", (req, res) => {
   let shortenedURL = generateRandomString(); //
-  urlDatabase[shortenedURL] = req.body.longURL; //maps shortLink to long link
-
+  urlDatabase[shortenedURL] = {
+    longURL: req.body.longURL,
+    userID: req.cookies["user_id"]
+  }
   res.redirect(302, `/urls/${shortenedURL}`);
 
 });
 
 app.get("/u/:shortURL", (req, res) => {
   //Redirects to long url
-  console.log(req.params.shortURL);
-  const longURL = urlDatabase[req.params.shortURL]
-  console.log(longURL);
+  let longURL;
 
+  const obj = urlDatabase[req.params.shortURL];
+  if (obj) {
+    longURL = obj.longURL;
+  } else {
+    longURL = undefined;
+  }
 
-
-  if (longURL !== undefined)
+  if (longURL !== undefined) {
     res.redirect(longURL);
+    return;
+  }
 
   res.send("URL NOT FOUND");
 });
@@ -215,8 +274,7 @@ const alreadyRegistered = function(userName) {
     }
   }
   return false;
-
-}
+};
 
 const findUserById = function(userID) {
   if (!userID) {
@@ -230,7 +288,7 @@ const findUserById = function(userID) {
   }
 
   return undefined;
-}
+};
 
 const findUserByEmail = function(email) {
   if (!email) {
@@ -243,8 +301,31 @@ const findUserByEmail = function(email) {
     }
   }
   return undefined;
-}
+};
 
 const passwordCheck = function(actual, guess) {
   return actual === guess;
+};
+
+const urlsForUser = function(id) {
+  //Given a user will find all urls that match for that user:
+  let retObj = {};
+  for (let shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) { //Only adds urls associated with the given user.
+      retObj[shortURL] = urlDatabase[shortURL].longURL;
+    }
+  }
+  return retObj;
 }
+/*
+const isLoggedIn = function(id) {
+  if (id === undefined) {
+    return false;
+  }
+  if (findUserById(id)) {
+    return true;
+  }
+  return false;
+}
+
+*/
